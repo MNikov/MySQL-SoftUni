0cfee6f434f7aad1ac7fd7aa6862dbf160fc01d1
Momchil Nikov <momchildnikov@gmail.com>
2022-02-05 23:52:09 +0200

    Update 10_get_users_photos_count.sql

--- Exams/Database_Basics_MySQL_Retake_Exam_31_March_2020/10_get_users_photos_count.sql
+++ Exams/Database_Basics_MySQL_Retake_Exam_31_March_2020/10_get_users_photos_count.sql
@@ -3,14 +3,14 @@ CREATE FUNCTION udf_users_photos_count(username VARCHAR(30))
 RETURNS INTEGER
 DETERMINISTIC
 BEGIN
-	SELECT 
+	RETURN (SELECT 
 		COUNT(up.photo_id)
 	FROM
 		users AS u
 			JOIN
 		users_photos AS up ON u.id = up.user_id
 	WHERE
-		u.username = username;
+		u.username = username);
 END $$;
 
 
